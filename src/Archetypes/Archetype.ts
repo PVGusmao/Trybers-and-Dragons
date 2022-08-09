09f63cc54f61b841a10ed269284c97a6e1f06039
Paulo Victor <pvmg1@hotmail.com>
2022-08-09 17:27:43 -0300

    Finalizado Requisito 04

---
+++ src/Archetypes/Archetype.ts
@@ -0,0 +1,26 @@
+import { EnergyType } from '../Energy';
+
+export default abstract class Archetype {
+  public special = 0;
+  public cost = 0;
+  
+  constructor(private _name: string) { }
+
+  get name(): string {
+    return this._name;
+  }
+
+  get getSpecial(): number {
+    return this.special;
+  }
+
+  get getCost(): number {
+    return this.cost;
+  }
+
+  static createdRacesInstances(): number {
+    throw new Error('Not implemented');
+  }
+
+  abstract get energyType(): EnergyType;
+}
