6e4757b2a13bfa1effefede5873879c3549a6c51
Paulo Victor <pvmg1@hotmail.com>
2022-08-10 13:07:18 -0300

    Finalziado Requisito 09

---
+++ src/Monster.ts
@@ -0,0 +1,34 @@
+import Fighter, { SimpleFighter } from './Fighter';
+
+export default class Monster implements SimpleFighter {
+  private _lifePoints: number;
+  private _strength: number;
+
+  constructor() {
+    this._lifePoints = 85;
+    this._strength = 63;
+  }
+
+  get lifePoints(): number {
+    return this._lifePoints;
+  }
+
+  get strength(): number {
+    return this._strength;
+  }
+
+  receiveDamage(attackPoints: number): number {
+    if (attackPoints > 0) {
+      this._lifePoints -= attackPoints;
+      if (this._lifePoints <= 0) {
+        this._lifePoints = -1;
+      }
+    }
+
+    return this._lifePoints;
+  }
+
+  attack(enemy: Fighter | SimpleFighter): void {
+    console.log(`${enemy} took ${this._strength} amount of damage`);
+  }
+}
